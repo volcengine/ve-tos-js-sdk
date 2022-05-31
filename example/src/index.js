f31291b39be7231f19d016079cd1b1db8529c0d7
Gang Chen <gangc.cxy@foxmail.com>
2022-05-31 19:48:26 +0800

    example add upload file

--- example/src/index.js
+++ example/src/index.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import TOS from '../../';
 
 const client = new TOS({
@@ -15,4 +16,18 @@ const jsonDom = document.querySelector('#json');
 listBucketsDom.addEventListener('click', async () => {
   const res = await client.listBuckets();
   jsonDom.innerHTML = JSON.stringify(res, null, 2);
+});
+
+const uploadObjectDom = document.querySelector('#upload-object');
+const fileDom = document.querySelector('input[type="file"]');
+uploadObjectDom.addEventListener('click', async () => {
+  const file = fileDom.files[0];
+  await client.putObject({ key: 'aaa', body: file, bucket: 'cg-beijing' });
+
+  // const url = client.getPreSignedUrl({ key: 'aaa', bucket: 'cg-beijing' });
+  // axios({
+  //   method: 'PUT',
+  //   url,
+  //   body: file,
+  // });
 });
